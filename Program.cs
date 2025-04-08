20b1bea57081e786d200968b9f473f4ec5a3a928
Yakup Bingöl <101745931+yakupbingol@users.noreply.github.com>
2025-04-08 15:48:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Methods2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int sonuc =Topla(7,3);
+            Console.WriteLine(sonuc);
+            Console.ReadLine();
+
+        }
+
+        static public int Topla(int sayi1, int sayi2)
+        {
+            double sonuc = Bol(45, 13);
+            return sayi1 + sayi2;
+        }
+
+         static public double Bol(double sayi1, double sayi2)
+        {
+            return sayi1 / sayi2;
+        }
+
+    }
+}
